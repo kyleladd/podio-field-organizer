8c2713972876eda16c782ac47284da196895ab4c
Kyle Ladd <kladd6@gmail.com>
2018-10-24 13:18:29 -0400

    getting/saving setting

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,21 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+var settings = [];
+chrome.storage.sync.get(['fields'], function (obj) {
+	settings = obj.fields;
+	console.log("settings",settings);
+});
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	// chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+  	if(request.action === "get-settings"){
+  		console.log("settings",settings);
+  		sendResponse(settings);
+  		return;
+  	}
+  	if(request.action === "set-settings"){
+  		debugger;
+  		chrome.storage.sync.set({"fields": request.settings});
+  		settings = request.settings;
+		sendResponse(settings);
+		return;
+  	}
   });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,31 +1,56 @@
+var settings = [];
+chrome.extension.sendMessage({"action": "get-settings", "app":"app"},
+  function (response) {
+    console.log("response",response);
+    settings = response;
+});
 chrome.extension.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-  if (document.readyState === "complete") {
-    clearInterval(readyStateCheckInterval);
-    // ----------------------------------------------------------
-    // This part of the script triggers when page is done loading
-    console.log("Hello. This message was sent from scripts/inject.js");
-    // ----------------------------------------------------------
+  $(function() {
     $(document).arrive(".app-fields-list > li", function() {
       var $container = $('.app-fields-list');
       var $fields = $container.find('> li').detach();
+    
+      // console.log("container",$container);
+      // console.log("FIELDS",$fields);
       $fields = $fields.sort(function(a, b) {
-        var aVal = $(a).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
-        var bVal = $(b).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
+        // var aVal = $(a).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
+        // var bVal = $(b).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
+        var aVal = settings.indexOf($(a).attr('id'));
+        var bVal = settings.indexOf($(b).attr('id'));
+        // console.log("A",aVal,"B",bVal);
+        if(aVal === -1 && bVal === -1){
+          return 0;
+        }
+        else if(aVal === -1){
+          return 1;
+        }
+        else if(bVal === -1){
+          return -1;
+        }
         if(aVal < bVal) { return -1; }
         if(aVal > bVal) { return 1; }
         return 0;
       });
-
+      console.log("$fields",$fields);
       $container.html($fields);
       var sortable = Sortable.create($container.get(0),{
         // Changed sorting within list
         onUpdate: function (evt) {
           // same properties as onEnd
-          console.log("sortable - onUpdate")
+          console.log("sortable - onUpdate", $fields);
+          // console.log("array",this.toArray())
+          console.log("new setting", $fields.map(function(){
+            return $(this).attr('id');
+          }).get());
+          debugger;
+          chrome.extension.sendMessage({"action": "set-settings", "settings": $fields.map(function(){
+            return $(this).attr('id');
+          }).get()},
+          function (response) {
+            console.log("response",response);
+          });
         }
       });
     });
-  }
-  }, 10);
+  });
 });
