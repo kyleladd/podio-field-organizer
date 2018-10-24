1507019dd8205b776a9eb672d4e0d6e5a7551908
Kyle Ladd <kladd6@gmail.com>
2018-10-24 01:04:25 -0400

    Added sortable

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,22 +7,24 @@ chrome.extension.sendMessage({}, function(response) {
     console.log("Hello. This message was sent from scripts/inject.js");
     // ----------------------------------------------------------
     $(document).arrive(".app-fields-list > li", function() {
-      // 'this' refers to the newly created element
-      // var $newElem = $(this);
       var $container = $('.app-fields-list');
-      var $fields = $('.app-fields-list > li').detach();
-      console.log("container",$container);
-      console.log("FIELDS",$fields);
+      var $fields = $container.find('> li').detach();
       $fields = $fields.sort(function(a, b) {
         var aVal = $(a).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
         var bVal = $(b).find('.label-content-wrapper .label-content').text().replace(/\*/g, '').replace(/^\s+|\s+$/g, '').toLowerCase();
-        console.log("A",aVal,"B",bVal);
         if(aVal < bVal) { return -1; }
         if(aVal > bVal) { return 1; }
         return 0;
       });
 
       $container.html($fields);
+      var sortable = Sortable.create($container.get(0),{
+        // Changed sorting within list
+        onUpdate: function (evt) {
+          // same properties as onEnd
+          console.log("sortable - onUpdate")
+        }
+      });
     });
   }
   }, 10);
